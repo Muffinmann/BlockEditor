72ff7fb52936dfc5793997c427a24224ef319455
conner <guanlai.zhang@orfo.de>
2023-11-16 10:56:16 -0600

    update: extract BasicBlock

---
+++ src/components/BasicBlock.tsx
@@ -0,0 +1,72 @@
+import { useId, useRef, useState, MouseEventHandler, ReactElement } from "react";
+import PopupSelect from "./PopupSelect";
+
+export interface BasicBlockProps {
+  displayName?: string,
+  type?: string, // block type: "if", "var", ...
+  disableAdd?: boolean,
+  disableRemove?: boolean,
+  className?: string, // supplement class names
+  onAdd?: (blockType: string) => void;
+  onRemove?: () => void;
+  children: ReactElement | ReactElement[]
+}
+
+const BasicBlock = ({
+  className = '',
+  displayName,
+  disableAdd,
+  disableRemove,
+  onAdd,
+  onRemove,
+  children }: BasicBlockProps) => {
+  const blockId = useId()
+  const blockRef = useRef(null)
+  const [hovered, setHovered] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleMouseOver: MouseEventHandler<HTMLDivElement> = (e) => {
+    e.stopPropagation(); // Prevent event from bubbling up to parent blocks
+    setHovered(true);
+  };
+
+  const handleMouseOut: MouseEventHandler<HTMLDivElement> = (e) => {
+    e.stopPropagation()
+    setHovered(false);
+    if (e.target === blockRef.current) {
+      setShowPopup(false)
+    }
+  };
+
+  return (
+    <div
+      ref={blockRef}
+      id={blockId}
+      className={`block ${className} ${hovered ? 'hovered' : ''}`}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+    >
+      <div className="block-header">{displayName}</div>
+      <div className="block-content">
+        {children}
+      </div>
+      <div className={`block-footer ${hovered ? '' : 'hidden'}`}>
+        {!disableAdd && (
+          <button
+            name="addButton"
+            className="add-btn"
+            onClick={() => { setShowPopup(true) }}
+          >
+            +
+          </button>)
+        }
+        {!disableRemove && (
+          <button className="remove-btn" onClick={onRemove}>-</button>
+        )}
+        {showPopup && <PopupSelect onClick={(blockType) => onAdd && onAdd(blockType)} />}
+      </div>
+    </div>
+  )
+};
+
+export default BasicBlock;
