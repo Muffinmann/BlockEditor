eccbb1424b7f7f4df78da81bc4664ad2cf05a8b1
conner <conner.zhang.gl@gmail.com>
2023-12-03 20:49:57 +0100

    update: translate popup when necessary

--- src/components/PopupSelect.tsx
+++ src/components/PopupSelect.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 import "./PopupSelect.css"
 import AVAILABLE_BLOCKS from '../config/blockDefinitions.json';
 import { BlockType } from "../types";
@@ -6,10 +6,38 @@ import { BlockType } from "../types";
 const PopupSelect = ({ onClick}: {onClick: (type: BlockType) => void}) => {
   const popupRef = useRef<HTMLDivElement | null>(null)
   const [searchKey, setSearchKey] = useState('')
-  // TODO: translate back to screen if it is out
+  const [translate, setTranslate] = useState({
+    x: 0,
+    y: 0,
+  })
+  // translate back to screen if it is out
+  useLayoutEffect(() => {
+    if (!popupRef.current) {
+      return;
+    }
+
+    const rect = popupRef.current.getBoundingClientRect()
+    const maxRight = window.innerWidth
+    const maxBottom = window.innerHeight
+    let translateX = 0
+    let translateY = 0
+    if (rect.bottom > maxBottom) {
+      translateY = maxBottom - rect.bottom
+    }
+    if (rect.right > maxRight) {
+      translateX = maxRight - rect.right
+    }
+    setTranslate({
+      x: translateX,
+      y: translateY
+    })
+  }, [])
   return ( 
     <div
       className="popup" id="popup" ref={popupRef}
+      style={{
+        transform: `translate(${translate.x}px, ${translate.y}px)`
+      }}
     >
       <div className="popup-header">
         <input
