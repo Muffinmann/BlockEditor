44bd95a7c465ed0cc5b1f17cb9a5141f1b112645
conner <conner.zhang.gl@gmail.com>
2024-01-28 11:34:22 +0100

    add tool bar item

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -6,16 +6,21 @@ import { BlockType } from "../types";
 
 interface Toolbar {
   onUpload: ChangeEventHandler<HTMLInputElement>,
+  onUploadKeywords: ChangeEventHandler<HTMLInputElement>,
   onDownload: MouseEventHandler<HTMLButtonElement>,
   onAddBlock: (type: BlockType) => void,
 }
 
-const Toolbar = ({onUpload, onDownload, onAddBlock}: Toolbar) => {
+const Toolbar = ({onUpload, onDownload, onUploadKeywords, onAddBlock}: Toolbar) => {
   const [showPopup, setShowPopup] = useState(false)
   return (
     <div className="toolbar">
+      <label htmlFor="keywordsUpload" className="toolbar-item">
+        <span className="icon"><FiUpload /></span> Keywords
+      </label>
+      <input onChange={onUploadKeywords} type="file" id='keywordsUpload' hidden />
       <label htmlFor="fileUpload" className="toolbar-item">
-        <span className="icon"><FiUpload /></span> Upload
+        <span className="icon"><FiUpload /></span> Upload Existing Block
       </label>
       <input onChange={onUpload} type="file" id="fileUpload" className="file-input" hidden />
       <button className="toolbar-item" type="button" onClick={onDownload}>
